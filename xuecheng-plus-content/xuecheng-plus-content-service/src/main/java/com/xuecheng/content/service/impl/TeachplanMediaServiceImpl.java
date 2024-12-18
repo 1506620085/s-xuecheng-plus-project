5afc058e3882244c367be6b49b2f39a45ea58049
Hang <1506620085@qq.com>
2024-12-18 10:30:42 +0800

    feat: ✨添加绑定媒资DAO、Service、实体类

---
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/impl/TeachplanMediaServiceImpl.java
@@ -0,0 +1,21 @@
+package com.xuecheng.content.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.xuecheng.content.model.po.TeachplanMedia;
+import com.xuecheng.content.service.TeachplanMediaService;
+import org.springframework.stereotype.Service;
+
+/**
+* @author Hang
+* @description 针对表【teachplan_media】的数据库操作Service实现
+* @createDate 2024-12-18 10:22:36
+*/
+@Service
+public class TeachplanMediaServiceImpl extends ServiceImpl<TeachplanMediaMapper, TeachplanMedia>
+    implements TeachplanMediaService{
+
+}
+
+
+
+
